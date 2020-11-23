6d0e744761bd61a98bad2fbb472304130527437c
Cuong VN <huucuongbeat95@gmail.com>
2020-11-24 00:43:40 +0700

    Use HashTable

---
+++ String/Duplicate/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// using HashTable
+int main()
+{
+    char A[]="AinzZAingzZ";
+    int H[53]={0},i;
+    for(i=0; A[i]!='\0'; i++) {
+        if(A[i]>=97 && A[i]<=122)
+            H[A[i]-97]+=1;
+        if(A[i]>=65 && A[i]<=90)
+            H[A[i]-38]+=1;
+    }
+    for(i=0; i<53; i++) {
+        if(H[i]>1 && i<27)
+            printf("%c appearing %d time\n", i+97, H[i]);
+        if(H[i]>1 && i>=27)
+            printf("%c appearing %d time\n", i+38, H[i]);
+    }
+
+    return 0;
+}

bb44f0b91ca9e7b77e9ccaea75f484b4c9bb50be
Cuong VN <huucuongbeat95@gmail.com>
2020-12-01 00:57:12 +0700

    Permutation of a given string using 2 ways

--- String/Permutation/main.c
+++ String/Permutation/main.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void perm(char s[], int k) {
+void swap(char *x, char *y) {
+    char temp;
+    temp=*x;
+    *x=*y;
+    *y=temp;
+}
+
+void perm1(char s[],int left, int right) {
+    int i;
+    if(left==right) {
+        printf("%s\n", s);
+    } else {
+        for(i=left; i<=right; i++) {
+            swap(&s[left], &s[i]);
+            perm1(s, left+1, right);
+            swap(&s[left], &s[i]);
+        }
+    }
+}
+
+void perm2(char s[], int k) {
     static int A[10]={0};
     static char res[10];
     int i;
@@ -13,7 +33,7 @@ void perm(char s[], int k) {
             if(A[i]==0) {
                 res[k]=s[i];
                 A[i]=1;
-                perm(s, k+1);
+                perm2(s, k+1);
                 A[i]=0;
             }
         }
@@ -22,7 +42,11 @@ void perm(char s[], int k) {
 
 int main()
 {
+    char d[]="ABCD";
+    perm1(d, 0, 3);
     char s[]="ABC";
-    perm(s,0);
+    perm2(s,0);
+
+
     return 0;
 }

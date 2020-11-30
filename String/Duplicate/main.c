894e8ccc63091f862028ecda2913f085fc928775
Cuong VN <huucuongbeat95@gmail.com>
2020-12-01 01:15:55 +0700

    Finding duplicates in a string using HashTable and bitwise operation

--- String/Duplicate/main.c
+++ String/Duplicate/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// using HashTable
 int main()
 {
+    // using HashTable
     char A[]="AinzZAingzZ";
     int H[53]={0},i;
     for(i=0; A[i]!='\0'; i++) {
@@ -18,6 +18,19 @@ int main()
         if(H[i]>1 && i>=27)
             printf("%c appearing %d time\n", i+38, H[i]);
     }
+
+    //using bitwise operation
+    char B[]="finding";
+    long int L=0,x=0;
+    for(int i=0; B[i]!='\0'; i++) {
+        x=1;
+        x <<= B[i]-97;
+        if((x&L)>0)
+            printf("%c is Duplicate\n", B[i]);
+        else
+            L = x|L;
+    }
+
 
     return 0;
 }
